6854feefcb2839b813149a9712ddd1fc4aa82efd
Victor Rian <victor.rian@hotmail.com>
2022-05-03 20:21:09 -0300

    Projeto de compra e venda de Acoes feito em VueJS

---
+++ src/components/stocks/Stock.vue
@@ -0,0 +1,56 @@
+<template>
+	<v-flex class="pr-3 pb-3" xs12 md6 lg4>
+		<v-card class="green darken-3 wjite--text">
+			<v-card-title class="headline">
+				<strong>{{ stock.name }} <small>(Preco: {{ stock.price | currency}})</small></strong>
+			</v-card-title>
+		</v-card>
+		<v-card>
+			<v-container fill-height>
+				<v-text-field label="Quantidade" type="number"
+					:error="insufficientFunds || !Number.isInteger(quantity)"
+				 	v-model.number="quantity"/>
+				<v-btn class="green darken-3 white--text"
+					:disabled="insufficientFunds || quantity <=0 || !Number.isInteger(quantity)"
+				 	@click="buystock">{{insufficientFunds ? "Insuficiente" : 'Comprar'}}</v-btn>
+
+			</v-container>
+		</v-card>
+	</v-flex>
+</template>
+
+<script>
+
+export default {
+	props: ['stock'],
+	data() {
+		return {
+			quantity: 0
+		}
+	},
+	computed: {
+		funds() {
+			return this.$store.getters.funds
+		},
+		insufficientFunds() {
+			return this.quantity * this.stock.price > this.funds		
+		}
+	},
+	methods: {
+		buystock() {
+			const order = {
+				stockId: this.stock.id,
+				stockPrice: this.stock.price,
+				quantity: this.quantity
+			}
+			this.$store.dispatch('buyStock', order)
+			this.quantity = 0
+			
+		}
+	}
+}
+</script>
+
+<style>
+
+</style>
